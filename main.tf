780eac84f61d58cba88d448bfaaaf5b371578bb8
Smaheshwar85 <44089003+Smaheshwar85@users.noreply.github.com>
2021-10-19 08:00:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "google" {
-  credentials ="iginite-world-ef6ceb4f00a5.json"
+  credentials =credentials("terraform-auth")
   project = "iginite-world" 
   region  = "us-central1"
   zone    = "us-central1-c"
 }
 
 resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance2"
+  name         = "terraform-instance3"
   machine_type = "f1-micro"
 
   boot_disk {

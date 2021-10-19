5056ea4285a6f45c4ac2139bf875f8330e25c775
Smaheshwar85 <44089003+Smaheshwar85@users.noreply.github.com>
2021-10-19 10:12:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,8 +21,31 @@ resource "google_compute_instance" "default" {
       }
     }
 }
-module "iam-roles"{
+/* module "iam-roles"{
 service_account =var.service_account
 roles   =var.bucket_roles
 prject_id =var.project
+} */
+resource "google_project_iam_binding" "project" {
+  project = var.project
+  role    = var.bucket_roles
+
+  members = [
+    "user:smahesh2305@gmail.com",
+  ]
+}
+
+
+resource "google_service_account" "sa" {
+  account_id   = var.service_account
+  display_name = "A service account that only mahesh can use"
+}
+
+resource "google_service_account_iam_binding" "admin-account-iam" {
+  service_account_id = google_service_account.sa.name
+  role               = var.bucket_roles
+
+  members = [
+    "user:smahesh2305@gmail.com",
+  ]
 }

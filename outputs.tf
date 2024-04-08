08cdb4f02517c26eb3a7f2dce9f4ad4befd9f254
shalini sharma <abc@gmail.com>
2024-04-09 04:28:50 +0530

    output file

--- outputs.tf
+++ outputs.tf
@@ -2,5 +2,7 @@
 output "sink_service_account_email" {
   value = module.logging_sink.sink_service_account_email
 }
-
+output "monitoring_dashboard_id" {
+  value = google_monitoring_dashboard.monitored_dash.id
+}
 
